72d5d5a253c8b2a451856cf94f19fd076f5cf915
Ricardo Morales Gonzalez <moralesgonzalezyb12@gmail.com>
2025-08-10 22:21:19 -0400

    Created skeleton for song link input

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from 'react'
 // will resolve in case there is any hard coded reference
 // const {BASE_URL} = import.meta.env
+import { SongInputForm } from "./components/example-form"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Spotify Song Info</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='bg-indigo-400 h-screen w-screen'>
+      <h1 className='text-3xl text-center pt-10'>Spotify Song Info App</h1>
+      <SongInputForm />
+    </div>
+      
+
     </>
   )
 }
---
+++ src/components/example-form.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import {z} from "zod"
+
+import {Button} from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {Input} from "@/components/ui/input"
+
+const formSchema = z.object({
+    song_link: z.url({protocol: /^https$/, hostname: /^open\.spotify\.com$/, message: "Invalid Spotify URL"})
+})
+
+export function SongInputForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            song_link: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                control={form.control}
+                name="song_link"
+                render={({field}) => (
+                    <FormItem>
+                        <FormLabel>Spotify Song Link</FormLabel>
+                        <FormControl>
+                            <Input {...field} />
+                        </FormControl>
+                        <FormDescription>
+                            We will get the metadata for the song you provide.
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )} 
+                
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
